688ce0e30bc6a05d1ab291bed6cb0092888a9f25
changyunyeong <sally0109277@naver.com>
2025-02-15 00:58:38 +0900

    ✨  feat: 헬스체크 api 구현 #1

---
+++ demo/src/main/java/prography/example/demo/domain/UserRoom/service/GameService.java
@@ -0,0 +1,8 @@
+package prography.example.demo.domain.UserRoom.service;
+
+import prography.example.demo.global.apiPayLoad.ApiResponse;
+
+public interface GameService {
+
+    ApiResponse<Void> checkHealth();
+}
