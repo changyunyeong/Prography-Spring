962e7be394d7532187d00477cb9dec2351bcfd93
changyunyeong <sally0109277@naver.com>
2025-02-14 20:27:08 +0900

    🎉  New Project

---
+++ demo/src/test/java/prography/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package prography.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
