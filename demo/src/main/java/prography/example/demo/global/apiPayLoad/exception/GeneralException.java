0c4f211619e5f1f34000ca08396f090c7c966826
changyunyeong <sally0109277@naver.com>
2025-02-16 20:39:27 +0900

    ✨  feat: 생성자 추가 #7

--- demo/src/main/java/prography/example/demo/global/apiPayLoad/exception/GeneralException.java
+++ demo/src/main/java/prography/example/demo/global/apiPayLoad/exception/GeneralException.java
@@ -6,10 +6,15 @@ import prography.example.demo.global.apiPayLoad.code.Base.BaseStatus;
 import prography.example.demo.global.apiPayLoad.code.Base.ReasonDTO;
 
 @Getter
-@AllArgsConstructor
 public class GeneralException extends RuntimeException {
 
     private BaseStatus status;
+
+    public GeneralException(BaseStatus status) {
+
+        super(status.getReason().getMessage());
+        this.status = status;
+    }
 
     public ReasonDTO getErrorReason() {
         return this.status.getReason();
