9008f0318e280d3a1fab514b0d07ddd23543daa4
changyunyeong <sally0109277@naver.com>
2025-02-16 20:38:44 +0900

    ⚡️  feat: 최초 1회만 호출하도록 수정 #7

---
+++ demo/src/main/java/prography/example/demo/global/runner/HealthCheckRunner.java
@@ -0,0 +1,21 @@
+package prography.example.demo.global.filter;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import prography.example.demo.domain.UserRoom.service.GameService;
+
+@Component
+public class HealthCheckRunner {
+
+    private final GameService gameService;
+
+    public HealthCheckRunner(GameService gameService) {
+        this.gameService = gameService;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        gameService.checkHealth();
+    }
+}
