9faa5f88a74c5acd81451b23f711ca75d382cb9e
changyunyeong <sally0109277@naver.com>
2025-02-17 12:18:34 +0900

    ✨  feat: swagger 설명 추가 #7

--- demo/src/main/java/prography/example/demo/domain/Room/controller/RoomController.java
+++ demo/src/main/java/prography/example/demo/domain/Room/controller/RoomController.java
@@ -1,5 +1,6 @@
 package prography.example.demo.domain.Room.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import prography.example.demo.domain.Room.converter.RoomConverter;
 import prography.example.demo.domain.Room.dto.request.RoomRequestDTO;
-import prography.example.demo.domain.Room.dto.response.RoomResponseDTO;
 import prography.example.demo.domain.Room.entity.Room;
 import prography.example.demo.domain.Room.service.RoomService;
 import prography.example.demo.global.apiPayLoad.ApiResponse;
@@ -21,10 +21,14 @@ public class RoomController {
     private final RoomService roomService;
 
     @PostMapping("/room")
+    @Operation(summary = "방 생성 API",
+            description = "방을 생성하려고 하는 user(userId)의 상태가 활성(ACTIVE)상태일 때만, 방을 생성할 수 있음\n"
+                + " 방을 생성하려고 하는 user(userId)가 현재 참여한 방이 있다면, 방을 생성할 수 없음")
     public ApiResponse<Void> createRoom (
             @RequestBody @Valid RoomRequestDTO.CreateRoomRequestDTO request) {
         Room room = roomService.createRoom(request);
         RoomConverter.toRoomResponseDTO(room);
         return ApiResponse.success(null);
     }
+
 }
