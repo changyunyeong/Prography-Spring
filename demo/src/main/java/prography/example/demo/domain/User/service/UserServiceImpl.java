9c9ad97040b244a09d765cdfeb2adc3704583ec8
changyunyeong <sally0109277@naver.com>
2025-02-17 12:37:39 +0900

    ✨  feat: fakerapi url 변경 #7

--- demo/src/main/java/prography/example/demo/domain/User/service/UserServiceImpl.java
+++ demo/src/main/java/prography/example/demo/domain/User/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package prography.example.demo.domain.User.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import prography.example.demo.domain.Room.repository.RoomRepository;
 import prography.example.demo.domain.User.converter.UserConverter;
 import prography.example.demo.domain.User.dto.response.UserResponseDTO;
@@ -21,7 +23,9 @@ import prography.example.demo.global.common.enums.UserStatus;
 
 import java.time.LocalDateTime;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,7 +37,8 @@ public class UserServiceImpl implements UserService {
     private final RoomRepository roomRepository;
     private final RestTemplate restTemplate;
 
-    private static final String FAKER_API_URL = "https://fakerapi.it/api/v1/users?_seed=%d&_quantity=%d&_locale=ko_KR";
+    @Value("${faker.api.url}")
+    private String FAKER_API_URL;
 
     @Override
     public void initUser(int seed, int quantity) {
@@ -42,7 +47,16 @@ public class UserServiceImpl implements UserService {
         roomRepository.deleteAll();
         userRepository.deleteAll();
 
-        String url = String.format(FAKER_API_URL, seed, quantity);
+        Map<String, Object> uriParams = new HashMap<>();
+        uriParams.put("seed", seed);
+        uriParams.put("quantity", quantity);
+
+//        String url = String.format(FAKER_API_URL, seed, quantity);
+
+        String url = UriComponentsBuilder.fromUriString(FAKER_API_URL)
+                .buildAndExpand(uriParams)
+                .toUriString();
+
         ResponseEntity<UserResponseDTO.UserListResponse> response = restTemplate.exchange(
                 url,
                 HttpMethod.GET,
