acbf173d6bfa884837193450768e2ef6a6c7ad00
changyunyeong <sally0109277@naver.com>
2025-02-18 22:26:00 +0900

    ✨  feat: 서버 에러 체크 응답 추가 #9

--- demo/src/main/java/prography/example/demo/global/runner/HealthCheckRunner.java
+++ demo/src/main/java/prography/example/demo/global/runner/HealthCheckRunner.java
@@ -1,11 +1,13 @@
 package prography.example.demo.global.runner;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import prography.example.demo.domain.UserRoom.service.GameService;
 
 @Component
+@Slf4j
 public class HealthCheckRunner {
 
     private final GameService gameService;
@@ -17,5 +19,6 @@ public class HealthCheckRunner {
     @EventListener(ApplicationReadyEvent.class)
     public void onApplicationReady() {
         gameService.checkHealth();
+        log.info("Application is ready");
     }
 }
